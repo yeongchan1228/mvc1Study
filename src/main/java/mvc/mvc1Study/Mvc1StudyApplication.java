b0532dab6a38a96a2c8af811e1e55e1bfa4bb7da
최영찬 <gks54410@gmail.com>
2022-03-28 20:03:55 +0900

    Feat: @ServletComponentScan 추가

--- src/main/java/mvc/mvc1Study/Mvc1StudyApplication.java
+++ src/main/java/mvc/mvc1Study/Mvc1StudyApplication.java
@@ -2,7 +2,9 @@ package mvc.mvc1Study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 public class Mvc1StudyApplication {
 
