721177a2cfc9097c1cd502b3f7428d00f89d5425
최영찬 <gks54410@gmail.com>
2022-03-29 21:25:44 +0900

    Feat: Json 형식의 요청 데이터 파싱할 객체 생성

---
+++ src/main/java/mvc/mvc1Study/servlet/HelloData.java
@@ -0,0 +1,11 @@
+package mvc.mvc1Study.servlet;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private String age;
+}
