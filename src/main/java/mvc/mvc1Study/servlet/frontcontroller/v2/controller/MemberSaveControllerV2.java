686701f1ac584cdecdfb812cb60e304e5e14c47c
최영찬 <gks54410@gmail.com>
2022-03-30 23:04:09 +0900

    Feat: 입력 받은 회원 정보를 저장한 후 저장된 회원을 출력하는 .jsp로 이동시키도록 .jsp 경로를 담고 있는 view 반환

---
+++ src/main/java/mvc/mvc1Study/servlet/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,30 @@
+package mvc.mvc1Study.servlet.frontcontroller.v2.controller;
+
+import mvc.mvc1Study.servlet.domain.member.entity.Member;
+import mvc.mvc1Study.servlet.domain.member.repository.MemberRepository;
+import mvc.mvc1Study.servlet.frontcontroller.MyView;
+import mvc.mvc1Study.servlet.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = Member.createMember().username(username).age(age).build();
+        memberRepository.save(member);
+
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save.jsp";
+        return new MyView(viewPath);
+    }
+}
