a8e8610d07cd5cb847d4677064cd9cdcaa24ef78
최영찬 <gks54410@gmail.com>
2022-03-30 23:33:49 +0900

    Feat: Model을 받아 request에 추가하고 forward하는 기능 추가

--- src/main/java/mvc/mvc1Study/servlet/frontcontroller/MyView.java
+++ src/main/java/mvc/mvc1Study/servlet/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -16,5 +17,12 @@ public class MyView {
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        model.forEach((key, value) -> req.setAttribute(key, value));
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
     }
 }
