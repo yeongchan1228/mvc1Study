7124413458525ddaca91c634d2528ea4e36af32f
최영찬 <gks54410@gmail.com>
2022-03-30 17:07:42 +0900

    Feat: MVC 패턴 회원 출력을 위한 멤버 조회 및 회원 출력을 위한 .jsp 파일로 이동 기능 추가

---
+++ src/main/java/mvc/mvc1Study/servlet/mvc/MvcMemberListServlet.java
@@ -0,0 +1,30 @@
+package mvc.mvc1Study.servlet.mvc;
+
+import mvc.mvc1Study.servlet.domain.member.entity.Member;
+import mvc.mvc1Study.servlet.domain.member.repository.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Member> all = memberRepository.findAll();
+        req.setAttribute("members", all);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
