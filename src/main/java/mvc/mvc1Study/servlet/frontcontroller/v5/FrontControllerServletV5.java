571ac6e20104b1f56b4da0f578c10758f4aded95
최영찬 <gks54410@gmail.com>
2022-03-31 17:11:13 +0900

    Feat: 입력 받은 Handler에 해당하는 Adapter를 찾아 실행한 후 render 하는 기능 추가

---
+++ src/main/java/mvc/mvc1Study/servlet/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,82 @@
+package mvc.mvc1Study.servlet.frontcontroller.v5;
+
+import mvc.mvc1Study.servlet.frontcontroller.ModelView;
+import mvc.mvc1Study.servlet.frontcontroller.MyView;
+import mvc.mvc1Study.servlet.frontcontroller.v3.controller.MemberFormControllerV3;
+import mvc.mvc1Study.servlet.frontcontroller.v3.controller.MemberListControllerV3;
+import mvc.mvc1Study.servlet.frontcontroller.v3.controller.MemberSaveControllerV3;
+import mvc.mvc1Study.servlet.frontcontroller.v5.adaptor.ControllerV3Adaptor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdaptor> handlerAdaptors = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+
+        initHandlerAdapters();
+    }
+
+    private void initHandlerAdapters() {
+        handlerAdaptors.add(new ControllerV3Adaptor());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Object handler = getHandler(req);
+
+        if(handler == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdaptor handlerAdapter = getHandlerAdapter(handler);
+
+        ModelView mv = handlerAdapter.handle(req, resp, handler);
+
+        String viewPath = mv.getViewName();
+        MyView view = viewResolver(viewPath);
+        view.render(mv.getModel(), req, resp);
+    }
+
+    private MyView viewResolver(String viewPath) {
+        MyView myView = new MyView("/WEB-INF/views/" + viewPath + ".jsp");
+        return myView;
+    }
+
+    private MyHandlerAdaptor getHandlerAdapter(Object handler) {
+        for (MyHandlerAdaptor handlerAdaptor : handlerAdaptors) {
+            if(handlerAdaptor.supports(handler)){
+                return handlerAdaptor;
+            }
+        }
+
+       throw new IllegalArgumentException("handler 찾기 오류");
+    }
+
+    private Object getHandler(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        Object handler = handlerMappingMap.get(requestURI);
+        return handler;
+    }
+}
